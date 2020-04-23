e588f9bc6deff511f408dd704185907fbeba1a9b
Challen <jianglun465@sina.com>
2020-04-23 08:32:53 +0800

    实现用户信息卡片，加了俩头像图片

--- src/stories/UserCard/CardSimple.js
+++ src/stories/UserCard/CardSimple.js
@@ -4,9 +4,46 @@ import styles from "./simple.module.css";
 import { action } from "@storybook/addon-actions";
 import IosMore from "react-ionicons/lib/IosMore";
 
-const CardSimple = ({ cardInfo }) => (
-  <article className={styles.card} onClick={action("clicked")}>
-    Simple
+const CardSimple = ({ userInfo }) => (
+  <article className={styles.card}>
+    <section className={styles.userbox}>
+      <img
+        src="/images/avatar.png"
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src = "/images/avatarph.jpg";
+        }}
+        className={styles.avatar}
+      />
+      <div className={styles.userinfo}>
+        <span className={styles.username}>Challen</span>
+        <span className={styles.usertitle}>游戏星球主宰</span>
+      </div>
+      <IosMore
+        onClick={action("clicked icons")}
+        fontSize="26px"
+        color="#C7C9D0"
+      />
+    </section>
+    <section className={styles.countbox}>
+      <span className={styles.counttext}>54k 赞</span>
+      <span className={styles.counttext}>394 粉丝</span>
+      <span className={styles.counttext}>53 碎片</span>
+    </section>
+    <section className={styles.buttonbox}>
+      <button
+        className={`${styles.buttons} ${styles.btnfollow}`}
+        onClick={action("clicked")}
+      >
+        关注
+      </button>
+      <button
+        className={`${styles.buttons} ${styles.btnmessage}`}
+        onClick={action("clicked")}
+      >
+        发消息
+      </button>
+    </section>
   </article>
 );
 
