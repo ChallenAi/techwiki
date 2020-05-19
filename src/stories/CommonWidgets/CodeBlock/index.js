f9c8da6daaf6c492f9b320df2aa475018519c10f
Challen <jianglun465@sina.com>
2020-05-19 16:55:02 +0800

    .

--- src/stories/CommonWidgets/CodeBlock/index.js
+++ src/stories/CommonWidgets/CodeBlock/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-// import { atomOneLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { arduinoLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import { githubGist } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export function CodeBlock({ language, value }) {
   if (!value) return null;
@@ -9,7 +10,7 @@ export function CodeBlock({ language, value }) {
   return (
     <SyntaxHighlighter
       language={language}
-      style={coy}
+      style={arduinoLight}
       customStyle={{
         backgroundColor: "transparent",
         overflowX: "scroll",
@@ -26,8 +27,12 @@ export function CodeInline({ language, value }) {
   return (
     <SyntaxHighlighter
       language={language}
-      style={coy}
+      style={arduinoLight}
       customStyle={{
+        // 这里的style仅用于
+        // 1. 覆盖 react-markdown自动给<pre>标签添加的内联样式。
+        // 2. inline和block两者<pre>标签间的直接的差异
+        // 比如内联溢出则换行，块溢出则左右滑动，但是无论哪种都不能显示滚动框
         display: "inline",
         backgroundColor: "transparent",
         wordWrap: "break-word",
