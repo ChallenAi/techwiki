d8f93a5488d347b7ac7fe87cfd9da15ea35fea14
Challen <jianglun465@sina.com>
2020-06-11 00:29:25 +0800

    实现我的星球UI

--- src/stories/Pages/MineCollections/index.js
+++ src/stories/Pages/MineCollections/index.js
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./collections.module.css";
 import MineHeader from "../../CommonWidgets/MineHeader";
+import { fetchMineCollections } from "../../../services/collection";
+import Masonry from "react-masonry-css";
+import CollectionCard from "../../CollectionCard/CardSimple";
 
 const MineCollections = () => {
-  const [collections, setcollections] = useState({});
-  //   useEffect(() => {
-  //     getcollectionsById({ collections_id: match.params.id })
-  //       .then((data) => setcollections(data))
-  //       .catch((err) => console.log(err));
-  //   }, [match.params.id]);
+  const [collections, setcollections] = useState([]);
+  const breakpointCols = {
+    default: 3,
+    // 1620: 3,
+    1300: 2,
+    860: 1,
+    // 1200: 2,
+    // 800: 1,
+  };
+
+  useEffect(() => {
+    fetchMineCollections()
+      .then((data) => {
+        setcollections(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={styles.box}>
       <MineHeader />
-      <div>我的星球</div>
+      <Masonry
+        breakpointCols={breakpointCols}
+        // breakpointCols={4}
+        className={styles.masonry}
+        columnClassName={styles.col}
+      >
+        {collections.map((info) => (
+          <CollectionCard key={info.collectionId} cardInfo={info} />
+        ))}
+      </Masonry>
     </div>
   );
 };
